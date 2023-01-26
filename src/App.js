ce3ae7888b513efb1dfca3e3cb0d870663542738
Todoran Marius <marius@purpletech.co.uk>
2023-01-26 02:15:41 +0200

    Improved landing page. Added steps when wallet not connected / insuffiecient funds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
-import { Image } from 'semantic-ui-react';
+import { Button, Image } from 'semantic-ui-react';
 import './App.css';
 import metamask from './assets/icons/metamask.svg';
+import gemini from './assets/icons/gemini.svg';
 import PurpleHeader from './components/header';
 import Main from './components/main';
 import fetchWallets from './hooks/fetchWallets';
@@ -9,35 +9,53 @@ import fetchWallets from './hooks/fetchWallets';
 import { WalletContext } from './hooks/walletContext';
 
 export default function App() {
-  const { wallet, setWallet, wallets, setWallets, hasMetaMask } = fetchWallets();
+  const { loading, balance, wallet, wallets, hasMetaMask } = fetchWallets();
 
-  useEffect(() => {
-    if (window.ethereum) {
-      window.ethereum.request({ method: 'eth_requestAccounts' }).then(res => {
-        setWallets(res);
-        setWallet(res[0]);
-      });
-    }
-    // eslint-disable-next-line
-  }, []);
-
-  if (hasMetaMask && !wallet) {
+  if (loading) {
     return <></>;
   }
 
   return (
     <>
-      <WalletContext.Provider value={{ wallet, setWallet, wallets, setWallets, hasMetaMask }}>
+      <WalletContext.Provider value={{ wallet, wallets, hasMetaMask }}>
         <PurpleHeader />
         <div className='purple-content'>
           {!hasMetaMask && <>
             <div className='custom-header'>
               <Image src={metamask} size="tiny" />
-              <span className='title'>MetaMask extension not available</span>
-              <span className='sub-title'>MetaMask is needed to join the sale. Install it from <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en" target="_blank" rel="noreferrer">here</a> or enable it</span>
+              <span className='title'>Metamask extension not available</span>
+              <span className='sub-title'>Metamask is needed to participate in the sale. Install it from <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en" target="_blank" rel="noreferrer">here</a> (or enable it if already installed)</span>
+            </div>
+            <div className='top-distance'><b>Supported browsers: Chrome, Firefox, Edge, and Brave</b></div>
+          </>}
+          {hasMetaMask && !wallet && <>
+            <div className='custom-header center-text'>
+              <Image src={metamask} size="tiny" />
+              <span className='title'>No Metamask wallet was connected</span>
+              <span className='sub-title'>Metamask installed but not connected, please follow the instructions on the metamask window to generate a wallet and then connect it</span>
+              <span className='top-distance sub-title'><b>Once you have successfully connected your wallet, the next step is to transfer Ethereum to your newly created wallet<br/>You must transfer at least 0.25 ETH in order to participate in the sale</b></span>
+              {/* <a className='margin-all' href="https://www.gemini.com/share/dz5e4za2l" target="_blank" rel="noreferrer">
+                <Button basic className='button-img' size='big'>
+                  <Image src={gemini} size="mini" />Buy Ethereum on GEMINI
+                </Button>
+              </a>
+              <b>(And receive $10 in BTC if you create a new account)</b> */}
             </div>
           </>}
-          {hasMetaMask && wallet && <div className='purple-content-inner'><Main /></div>}
+          {hasMetaMask && wallet && balance === 0 && <>
+            <div className='custom-header center-text'>
+              <Image src={metamask} size="tiny" />
+              <span className='title'>Insufficient funds</span>
+              <span className='sub-title'>You must transfer <b>at least 0.25 ETH</b> to your wallet in order to participate in the sale</span>
+              <a className='top-distance' href="https://www.gemini.com/share/dz5e4za2l" target="_blank" rel="noreferrer">
+                <Button basic className='button-img margin-all' size='big'>
+                  <Image src={gemini} size="mini" />Buy Ethereum on GEMINI
+                </Button>
+              </a>
+              <b>(And receive $10 in BTC if you create a new account)</b>
+            </div>
+          </>}
+          {hasMetaMask && wallet && balance !== 0 && <div className='purple-content-inner'><Main /></div>}
         </div>
       </WalletContext.Provider>
     </>
--- src/hooks/fetchWallets.js
+++ src/hooks/fetchWallets.js
@@ -1,21 +1,44 @@
 import { useEffect, useState } from "react";
 
 export default function FetchWallets() {
+  const [loading, setLoading] = useState(true);
+  const [balance, setBalance] = useState(null);
   const [wallet, setWallet] = useState(null);
   const [wallets, setWallets] = useState(null);
   const [hasMetaMask, setHasMetaMask] = useState(false);
 
   useEffect(() => {
-    if (window.ethereum) {
+    const fn = async () => {
+      if (!window.ethereum) {
+        setHasMetaMask(false);
+        setLoading(false);
+        return;
+      }
+
       setHasMetaMask(true);
-    }
+
+      try {
+        const res = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setWallets(res);
+        setWallet(res[0]);
+
+        if (res[0]) {
+          const b = await window.ethereum.request({ method: 'eth_getBalance', params: [res[0], 'latest'] });
+          setBalance(parseInt(b.substring(2), 16) / 1000000000000000000);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fn();
   }, [])
 
   return {
+    loading,
     wallet,
-    setWallet,
     wallets,
-    setWallets,
     hasMetaMask,
+    balance
   };
 }
